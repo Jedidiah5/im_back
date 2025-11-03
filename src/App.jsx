33aa4443735906c888db34c9b6df1aa5ca0831c3
enesi <jedidiahonotu@gmail.com>
2025-11-03 14:10:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Form />
+    </div>
+  )
+}
+
+export default App 
---
+++ src/componets/form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function form() {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Enter your name" />
+        <input type="email" placeholder="Enter your email" />
+        <input type="password" placeholder="Enter your password" />
+        <input type="submit" value="Submit" />
+      </div>
+    </div>
+  )
+}
+
+export default form
